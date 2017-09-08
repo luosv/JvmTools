520c5790efc1b84f6bef1d45d91bd6ae951f30d1
luosv <luo_shaowei@outlook.com>
2017-09-08 23:43:05 +0800

    debug:UserServiceImpl需要实现IUserService接口，但是教程上的代码并没有实现，原因还需探究

--- src/main/java/com/jvmTools/service/impl/UserServiceImpl.java
+++ src/main/java/com/jvmTools/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.jvmTools.service.impl;
 
 import com.jvmTools.dao.IUserDao;
 import com.jvmTools.model.User;
+import com.jvmTools.service.IUserService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -10,7 +11,7 @@ import javax.annotation.Resource;
  * Created by luosv on 2017/9/8 0008.
  */
 @Service("userService")
-public class UserServiceImpl {
+public class UserServiceImpl implements IUserService {
 
     @Resource
     private IUserDao userDao;
